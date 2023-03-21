7e49eb2eb6848ee680849932526d3ee6021b914e
ToKiodfm <101168277+ToKiodfm@users.noreply.github.com>
2023-03-21 03:36:07 -0300

    auth/ front

--- src/main/java/com/portfolio/HM/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/HM/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://backendaphm.onrender.com")
+@CrossOrigin(origins = "https://proyectofinal-26481.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
